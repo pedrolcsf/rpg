fd05807559bc0d487277526f75280a480a033df6
Pedro Ferreira <pedrolcsferreira@gmail.com>
2021-12-14 14:24:39 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <Head>
+      <title>RPG - System</title>
+      <meta name="description" content="RPG System." />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
